e3df5f0c6b56fa2e702407979e68aba39445fe3a
Saju-SM <114918153+Saju-SM@users.noreply.github.com>
2022-10-03 20:35:33 +0600

    Update SM.py

--- SM.py
+++ SM.py
@@ -225,7 +225,7 @@ def bot():
 def menu():
 	os.system('clear')
 	banner()
-	os.system("xdg-open https://www.facebook.com/owner.termux")        
+	os.system("xdg-open https://www.facebook.com/Md.Almutasimsaju.Simple.man143")        
 	print(f'\033[1;37m()==============================================()    ')
 	print('\t\x1b[1;97m[\x1b[1;92m M A I N - M E N U  \x1b[1;97m]')
 	print(f'\033[1;37m()==============================================()    ')
